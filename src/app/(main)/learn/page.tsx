0c59019dfa7936287dde7c0a54181c4e7ef3880a
JeikuDevv <jakelourencevillar.dev@gmail.com>
2024-03-22 19:48:29 +0800

    wrappers

--- src/app/(main)/learn/page.tsx
+++ src/app/(main)/learn/page.tsx
@@ -1,7 +1,10 @@
+import { StickyWrapper, FeedWrapper } from "@/components";
+
 const LearnPage = () => {
   return (
-    <div>
-      <p>LEARNING</p>
+    <div className="flex flex-row-reverse gap-48 px-6">
+      <StickyWrapper>MY STICKY SIDE BAR</StickyWrapper>
+      <FeedWrapper>MY FEED SIDE BAR</FeedWrapper>
     </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 
 export { Sidebar } from './sidebar'
 export { MobileHeader } from './mobile-header'
+export { MobileSidebar } from './mobile-sidebar'
+export { SidebarItem } from './sidebar-item'
+export { StickyWrapper } from './sticky-wrapper'
+export { FeedWrapper } from './feed-wrapper'
